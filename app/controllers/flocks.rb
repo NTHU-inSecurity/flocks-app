995096300017b7d30d4de16cb36d2ce823daeabb
carrie0925 <ca0983896371@gmail.com>
2025-05-17 23:56:10 +0800

    add in App to view index of resources

--- app/controllers/flocks.rb
+++ app/controllers/flocks.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'roda'
-require_relative './app'
+require_relative 'app'
 
 module Flocks
   class App < Roda
@@ -12,6 +12,19 @@ module Flocks
         view :flocks, locals: { flocks:, current_account: @current_account }
       end
 
+      # create：GET /flock/my - current_account flocks
+      routing.is 'my' do
+        # check if the user is logged in
+        unless @current_account&.logged_in?
+          flash[:error] = 'please login first'
+          routing.redirect '/auth/login'
+        end
+        # user flocks
+        flocks = FlocksServices::GetUserFlocks.new(App.config)
+                                              .call(@current_account)
+        view :my_flocks, locals: { flocks:, current_account: @current_account }
+      end
+
       # GET and POST /flock/create
       routing.is 'create' do
         routing.get do
@@ -19,8 +32,20 @@ module Flocks
         end
 
         routing.post do
+          # check if the user is logged in
+          unless @current_account&.logged_in?
+            flash[:error] = '請先登入來創建新資源'
+            routing.redirect '/auth/login'
+          end
+
           destination_url = routing.params['destination_url']
-          new_flock = FlocksServices::CreateFlock.new(App.config).call(destination_url:)
+
+          # use auth token to create a new flock
+          new_flock = FlocksServices::CreateFlock.new(App.config)
+                                                 .call(
+                                                   destination_url:,
+                                                   current_account: @current_account
+                                                 )
 
           flash[:notice] = 'Flock created successfully'
           routing.redirect '/flock/all'
--- app/services/authenticate_account.rb
+++ app/services/authenticate_account.rb
@@ -3,21 +3,34 @@
 require 'http'
 
 module Flocks
-  # Returns an authenticated user, or nil
-  class AuthenticateAccount
-    class UnauthorizedError < StandardError; end
-
-    def initialize(config)
-      @config = config
+  # Error for invalid credentials
+  class UnauthorizedError < StandardError
+    def initialize(msg = nil)
+      super
+      @credentials = msg
     end
 
-    def call(email:, password:)
-      response = HTTP.post("#{@config.API_URL}/auth/authenticate",
-                           json: { email:, password: })
+    def message
+      if @credentials && @credentials[:username]
+        "Invalid Credentials for: #{@credentials[:username]}"
+      else
+        'Invalid Authorization Token'
+      end
+    end
+  end
 
-      raise(UnauthorizedError) unless response.code == 200
-
-      response.parse['attributes']
+  # Find account and check password
+  class AuthenticateAccount
+    def self.call(credentials)
+      if credentials[:auth_token]
+        account = Account.first(auth_token: credentials[:auth_token])
+        account || raise
+      else
+        account = Account.first(username: credentials[:username])
+        account.password?(credentials[:password]) ? account : raise
+      end
+    rescue StandardError
+      raise UnauthorizedError, credentials
     end
   end
 end
