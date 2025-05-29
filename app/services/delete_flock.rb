f29dfabd8d70e377ff507cccbcda9874e827afea
syaheer-altaf <altafsyaheer@gmail.com>
2025-05-30 03:08:03 +0800

    modified: flocks/delete implementation

--- app/services/delete_flock.rb
+++ app/services/delete_flock.rb
@@ -10,10 +10,22 @@ module Flocks
       end
 
       def call(flock_id, current_account)
-        response = HTTP.auth("Bearer #{current_account.auth_token}")
-                       .delete("#{@config.API_URL}/flocks/#{flock_id}")
+        # birds_response = HTTP.auth("Bearer #{current_account.auth_token}")
+        #                      .get("#{@config.API_URL}/flocks/#{flock_id}/birds")
 
-        raise response.parse['message'] || 'Failed to delete flock' unless response.code == 200
+        # raise 'Could not retrieve bird data' unless birds_response.code == 200
+        # birds = birds_response.parse['data']
+
+        # unless birds.any? { |bird|
+        #   bird.dig('included', 'account', 'attributes', 'username') == current_account.username
+        # }
+        #   raise 'You are not a member of this flock'
+        # end
+
+        response = HTTP.auth("Bearer #{current_account.auth_token}")
+                       .post("#{@config.API_URL}/bird/#{flock_id}")
+
+        raise response.parse['message'] || 'Failed to delete or exit flock' unless response.code == 200
 
         true
       end
